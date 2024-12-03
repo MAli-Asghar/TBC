e390172e736c4f54266d50f41f92a87b73f880a2
MAli-Asghar <ali.asghar@magna.com>
2024-12-03 11:30:07 +0100

    TBC Description

---
+++ test_benches.py
@@ -0,0 +1,7 @@
+projects = {
+    "BMW AdCAM": ["FuSa MEEDTTENV123 ", "Healing : MEEDTTENV35", "MEEDTXTENV36", "MEEDTXTENV38", "MEEDT0004", "MEEDTTENV91" , "MEEDTTENV122" ],
+    "VW LiDAR": [],
+    "VW MIK": []
+}
+
+print(f"Test Benches for the projects are:\n{projects}")
